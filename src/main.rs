512217584b3c74275d61f1b42fdbd5b006d92e15
Daniel Gipps <daniel.gipps@properly.ca>
2023-03-07 13:01:58 -0500

    Refactor project to separate routes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -58,10 +58,9 @@ async fn subscribe_returns_400_for_data_problems() {
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
