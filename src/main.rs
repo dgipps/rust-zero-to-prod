61105114bf3cdbb0f82f4d458fc9de4ddc8686e6
Daniel Gipps <daniel.gipps@properly.ca>
2023-03-07 13:27:26 -0500

    Setup sqlx and add CI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
     run(listener)?.await
 }
